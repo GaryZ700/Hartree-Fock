2689bd9a1f1b6aa8948a633fb22cf231c4e74a2f
Gary <gzeri@uci.edu>
2017-10-02 19:56:27 -0700

    Adds PyQuante integrals

--- HF.py
+++ HF.py
@@ -10,6 +10,12 @@ from basis import Basis
 from tests.testValues import testSCF
 from integrals import Integrals
 
+#import PyQuante to handle integrals
+from PyQuante import Molecule
+from PyQuante import Ints
+from PyQuante import fermi_dirac as pyqSCF
+from PyQuante import LA2 
+
 scf = SCF() 
 basis = Basis()
 testValues = testSCF()
@@ -33,9 +39,17 @@ system = {
         }
 
 #build basis set for system
-basisSet = basis.buildBasis(system)
+#basisSet = basis.buildBasis(system)
 
-print(basisSet)
+molec = Molecule( 
+        
+        "H",
+        [  (1, (0,0,0) ),
+            (1,(0,0,1))]
+
+        )
+
+basisSet = Ints.getbasis(molec)
 
 #scf energy limit
 E_limit = 1.0 * pow(10.0, -6)
@@ -44,31 +58,37 @@ E_limit = 1.0 * pow(10.0, -6)
 #main code goes here
 
 #init integrals
-S = integrals.overlap(basisSet)
-T = integrals.KE(basisSet)
-Vext = integrals.nucAttract(basisSet, system["Z"], system["R"])
-G = integrals.elecRepulsion(basisSet)
+#S = integrals.overlap(basisSet)
+#T = integrals.KE(basisSet)
+#Vext = integrals.nucAttract(basisSet, system["Z"], system["R"])
+#G = integrals.elecRepulsion(basisSet)
+
+#pyquante integrals
+S = np.asmatrix(Ints.getS(basisSet))
+T = np.asmatrix(Ints.getT(basisSet))
+Vext = np.asmatrix(Ints.getV(basisSet, molec))
+G = Ints.get2ints(basisSet)
 
 print("Integrals integrated \n")
-
+#
 print("\n Overlap matrix")
 print(S)
 print("overlap end")
-
+#
 print("\n KE matrix")
 print(T)
 print("KE end")
-
+#
 print("\n Vext matrix")
 print(Vext)
 print("Vext end")
-
+#
 print("\n G matrix")
 print(G)
 print("G end")
-
+#
 #init test operators to check program is working
-#S, Vext, T = testValues.testerParse()
+#S, Vext, T, G = testValues.testerParse()
 
 #hamiltonian is equal to kinetic energy plus external potential
 #pg.176 Equ. 3.233
@@ -109,9 +129,9 @@ while(0 == 0):
         FMO = X.conjugate().transpose() * F * X
         FMO = scf.zero(FMO)
 
-#        print("FMO")
-#        print(FMO)
-#        print("\n")
+        print("FMO")
+        print(FMO)
+        print("\n")
         print("Fock matrix transformed")
 
         #8
@@ -119,28 +139,35 @@ while(0 == 0):
         #and get eigen value matrix
         eVal, eVec = np.linalg.eigh(FMO)
     
-        #print(eVec)
-        #print(eVal)
+        print(eVec)
+        print(eVal)
 
         #9
         #transform eigen vectors to AO basis
         eVecAO = np.dot(X, eVec)
         
-#        print("eVec AO")
-#        print(eVecAO)
-#        print("\n")
+        print("eVec AO")
+        print(eVecAO)
+        print("\n")
             
         #calculate density matrix
-        D = scf.buildDensity(eVecAO, system["N"])
+        De = scf.buildDensity(eVecAO, system["N"])
         
+        nclosed,nopen = molec.get_closedopen()
+        nocc = nclosed
+        S, h, Intss = Ints.getints(basisSet, molec)
+        eVal, eVecAO = LA2.geigh(Hcore, S)
+
+        D = pyqSCF.mkdens(eVecAO,34,nocc)
+
         print("Density built")
 
-#        print("Density")
-#        print(D)
-#        print("\n")
+        print("Density")
+        print(D)
+        print("\n")
        
         #calculate electronic energy
-        E.append(scf.energy(Hcore, F, D))
+        E.append(scf.energy(Hcore, F, De))
         
         #increment cycle counter
         cycle += 1
@@ -148,14 +175,17 @@ while(0 == 0):
         #check convergence
         if(abs(E[cycle] - E[cycle-1]) <= E_limit):
             print("energy converged")
-            break
+            #break
         
         print("\nEnergy  "  + str(E[cycle]))
 
         #if convergence did not occur,
         #rebuild Fock Matrix using Hartree Exchange Energy
 
-        htEx = integrals.HTExchange(D, G)
+        #htEx = integrals.HTExchange(D, G)
+
+        htEx = Ints.get2JmK(G, D)
+
         F = Hcore + htEx
 
 print(E[cycle])
